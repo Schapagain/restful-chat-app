0053ef5c681030befab09e0d05588db7fda9beb3
Sandesh C <sandesh@greatbear.tech>
2020-10-06 10:39:02 -0400

    add morgan to log incoming requests

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const userRoutes = require('./api/routes/users');
 const chatRoutes = require('./api/routes/chats');
+
+// Log all incoming requests before handling them
+app.use(morgan('dev'));
 
 app.use('/users', userRoutes);
 app.use('/chats',chatRoutes);
