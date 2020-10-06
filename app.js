697fbde7e21b7c280de880d3f82fede369131f3c
Sandesh C <sandesh@greatbear.tech>
2020-10-06 10:45:37 -0400

    add error handler to invalid routes

--- app.js
+++ app.js
@@ -8,7 +8,26 @@ const chatRoutes = require('./api/routes/chats');
 // Log all incoming requests before handling them
 app.use(morgan('dev'));
 
+
+// Use middleware to handle valid routes
 app.use('/users', userRoutes);
 app.use('/chats',chatRoutes);
+
+// Forward invalid routes to the error handler below
+app.use((req,res,next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+// Handle all errors throw
+app.use((error,req,res,next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+        }
+    });
+});
 
 module.exports = app;
