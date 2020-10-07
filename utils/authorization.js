c3b43ac6d3544ccfadb706589660df6dc5519c2c
Sandesh C <sandesh@greatbear.tech>
2020-10-07 16:34:53 -0400

    extend token expiry to an hour

--- utils/authorization.js
+++ utils/authorization.js
@@ -8,7 +8,7 @@ const getAuthToken = username => {
       sub: username,
     }
     const token = njwt.create(claims,signingKey);
-    token.setExpiration(new Date().getTime() + (60*1000));
+    token.setExpiration(new Date().getTime() + (60*60*1000));
     return token.compact();
 }
 
