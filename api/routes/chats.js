886d0feb3970c4f33ead7b564c1d8031a8538a99
Sandesh C <sandesh@greatbear.tech>
2020-10-07 08:26:58 -0400

    implement get/post chats by sender/receiever ids

--- api/routes/chats.js
+++ api/routes/chats.js
@@ -1,43 +1,147 @@
+
 const path = require('path');
 const express = require('express');
 const router = express.Router();
+const secureRandom = require('secure-random');
 
 // Database connection
+// Using 123 as an id for everyone for now
+// this will be username for users after login
 const appRoot = path.dirname(require.main.filename);
 const db = require(appRoot.concat('/db'));
 
 router.get('/',(req,res,next) => {
-    res.status(200).json({
-        message: "Chats were fetched"
+    const queryString = "SELECT * FROM chats";
+
+    db.query(queryString)
+    .then(result => {
+        res.status(201).json({
+            message: "These are all the chats:",
+            users: result.rows,
+        });
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err.message,
+        });
+    }); 
 });
 
 router.post('/',(req,res,next) => {
-    const chat = {
-        sender: req.body.sender,
-        message: req.body.message, 
+    ({sender,receiver,message} = req.body);
+
+    if (!sender || !receiver){
+        throw new Error('Sender and receiver are required');
     }
-    console.log(chat);
-    res.status(201).json({
-        message: "Chat was created",
-        chat,
+    const queryString = "INSERT INTO chats (sender,message,receiver) VALUES ($1,$2,$3) RETURNING *";
+    const queryValues = [sender,message,receiver];
+
+    checkUserExistence(sender)
+    .then( ()=> {
+        return checkUserExistence(receiver)
+        .then( () => {
+            db
+            .query(queryString,queryValues)
+            .then(result => {
+                res.status(201).json({
+                    message: "Chat added successfully",
+                    chat: result.rows[0],
+                })
+            })
+        })
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            message: "Could not add chat",
+            error: err.message,
+        })
+    }); 
+
 });
 
-router.get('/:chatId', (req,res,next) => {
-    const id = req.params.chatId;
-    res.status(200).json({
-        id,
-        message: 'You chat the got!'
+router.get('/:username', (req,res,next) => {
+    const username = req.params.username;
+
+    checkUserExistence(username)
+    .then( () => {
+        getUserChats(username)
+        .then(result => {
+            res.status(201).json({
+                message: "These are all the chats for this user",
+                chats: result,
+            });
+        })
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err.message,
+        });
+    });
+    
 });
 
-router.delete('/:chatId', (req,res,next) => {
-    const id = req.params.chatId;
-    res.status(200).json({
-        id,
-        message: 'Deleted chat!'
+const getUserChats = username => {
+    const allChats = {};
+    return getChatsFromDb(username,true)
+    .then (result => {
+        allChats.sent = result.rows;
+        return getChatsFromDb(username,false)
+        .then (result => {
+            allChats.received = result.rows;
+            return allChats;
+        })
     })
-});
+    .catch( err => {
+        console.log(err)
+    })
+}
+
+const getChatsFromDb = (username,isSender) => {
+    const queryString =`SELECT * FROM chats WHERE ${isSender?'sender':'receiver'}=$1`;
+    const queryValues = [username];
+    return db.query(queryString,queryValues)
+}
+
+const checkUserExistence = username => {
+    const queryString = "SELECT username FROM login WHERE username=$1";
+    const queryValues = [username];
+    return db
+        .query(queryString,queryValues)
+        .then( result => {
+            if (result.rowCount <= 0) {
+                throw new Error('User does not exist');
+            }
+        })
+}
+
+// router.delete('/:username', (req,res,next) => {
+//     const username = req.params.username;
+//     const queryString = "DELETE FROM users WHERE username=$1 RETURNING *";
+//     const queryValues = [username];
+
+//     db.query(queryString,queryValues)
+//     .then( result => {
+
+//         if (!result.rowCount){
+//             res.status(404).json({
+//                 message: "User not found",
+//             });
+//         }else{
+//             res.status(201).json({
+//                 message: "User was removed successfully",
+//                 user: result.rows[0],
+//             });
+//         }
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json({
+//             error: err,
+//         });
+//     });
+// });
 
 module.exports = router;
