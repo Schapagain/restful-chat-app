5ae30d47eff1b639a167379c80abeb66deeaca00
Sandesh C <sandesh@greatbear.tech>
2020-10-06 21:38:50 -0400

    respond with error message instead of the whole stack

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((error,req,res,next) => {
     res.status(error.status || 500);
     res.json({
         error: {
-            message: error.stack,
+            message: error.message,
         }
     });
 });
