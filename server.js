b7ad8b8e1c06fd8237a12b4bd6ac302dfe8364cf
Sandesh C <sandesh@greatbear.tech>
2020-10-07 21:36:12 -0400

    handle socket registration and login

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const http = require('http');
 const app = require('./app');
+const socketio = require('socket.io');
+
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
-
+const io = socketio(server);
 server.listen(port);
+
+
+// Imports for socket
+// Refactor later
+const { register_socket } = require('./api/drivers/register');
+const { login_socket } = require('./api/drivers/login');
+
+
+io.on('connection',socket => {
+    console.log('Someone connected!');
+
+    socket.on('registration', user => {
+        register_socket(socket,user);
+    })
+
+    socket.on('login', user => {
+        login_socket(socket,user);
+    })
+
+})
+
