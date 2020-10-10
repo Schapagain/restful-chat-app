6977376e84e78d79971c4159c31c8cbe376e5237
Sandesh C <sandesh@greatbear.tech>
2020-10-10 17:25:25 -0400

    handle image uploads

--- public/chat/chat.js
+++ public/chat/chat.js
@@ -6,6 +6,13 @@ $('form').submit((e)  => {
     return false;
 });
 
+$('#image').change( e => {
+    e.preventDefault();
+    console.log('uploaded an image');
+    handleOutgoingImage();
+    return false;
+})
+
 socket.on('broadcast-message',msg => {
     handleBroadcastMsg(msg);
 })
@@ -18,12 +25,24 @@ socket.on('authorization-failure', result => {
     console.log(result);
 })
 
-const handleIncomingChatMsg = (msg) => {
+const handleIncomingChatMsg = msg => {
 
-    const messageDiv = $('<li class = "other-message">');
-    const metaMesage = $('<div class = "meta-message">').text(msg.username.concat(' ',getCurrentTime()));
-    const textmessage = $('<div class = "message">').text(msg.message);
-    $('#messages').append(messageDiv.append(metaMesage,textmessage));
+    if (msg.image) {
+        const messageDiv = $('<div class="other-message">');
+        let metaMesage = $('<div class = "meta-message">').text(msg.username.concat(' ',getCurrentTime())); 
+        const newImage = $('<img>');
+        newImage.attr('src',msg.image);
+        $('#messages').append(messageDiv.append(metaMesage,newImage));
+        newImage[0].scrollIntoView();
+    }
+    if (msg.message) {
+        messageDiv = $('<div class = "other-message">');
+        metaMesage = $('<div class = "meta-message">').text(msg.username.concat(' ',getCurrentTime()));
+        const textmessage = $('<div class = "message">').text(msg.message);
+        $('#messages').append(messageDiv.append(metaMesage,textmessage));
+        textmessage[0].scrollIntoView();
+    }
+    
 }
 
 const handleOutgoingChatMsg = () => {
@@ -32,16 +51,46 @@ const handleOutgoingChatMsg = () => {
     socket.emit('chat-message',{message,userToken});
     $('#message').val('');
 
-    const messageDiv = $('<li class = "own-message">');
+    const messageDiv = $('<div class = "own-message">');
     const metaMesage = $('<div class = "meta-message">').text(getCurrentTime());
     const textmessage = $('<div class = "message">').text(message);
     
     // Add ownmessage to the chat-box
     $('#messages').append(messageDiv.append(metaMesage,textmessage));
+    messageDiv[0].scrollIntoView();
+}
+
+const handleOutgoingImage = () => {
+    const img = $('#image').prop('files')[0];
+    $("#image").replaceWith($("#image").val('').clone(true));
+
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+        const base64String = fileReader.result;
+        const userToken = getUserToken();
+        socket.emit('chat-message',{
+            image: base64String,
+            userToken,
+        });
+        const messageDiv = $('<div class = "own-message">');
+        const metaMesage = $('<div class = "meta-message">').text(getCurrentTime());
+        const newImage = $('<img>').attr('src',base64String);
+        
+        // Add own image to the chat-box
+        $('#messages').append(messageDiv.append(metaMesage,newImage));
+        messageDiv[0].scrollIntoView();
+
+    }
+    fileReader.readAsDataURL(img);
+
 }
 
 const handleBroadcastMsg = msg => {
-    $('#messages').append($('<div class="broadcast-message">').text(msg.message));
+    messageDiv = $('<div class = "broadcast-message">');
+    metaMesage = $('<div class = "meta-message">').text(msg.username.concat(' ',getCurrentTime()));
+    const textmessage = $('<div class = "message">').text(msg.message);
+    $('#messages').append(messageDiv.append(metaMesage,textmessage)); 
+    messageDiv[0].scrollIntoView();
 }
 
 const getUserToken = () => {
@@ -57,11 +106,13 @@ const getUserProfile = userToken => {
     socket.emit('get-user', userToken);
 };
 
-socket.on('get-user-response', user => {
-    const profilePicture = user.user.profilepicture;
-    $('#profile-pic').attr('src',profilePicture);
-    
-})
-
 // Load user profile
 getUserProfile(getUserToken());
+
+socket.on('get-user-response', user => {
+    if (user.success){
+        const profilePicture = user.user.profilepicture;
+        $('#profile-pic').attr('src',profilePicture);
+    }
+})
+
