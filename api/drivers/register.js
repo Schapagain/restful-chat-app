ea1ded5cc6ae075113515a3d4147d502285aa5ca
Sandesh C <sandesh@greatbear.tech>
2020-10-10 17:24:49 -0400

    fix minor typo

--- api/drivers/register.js
+++ api/drivers/register.js
@@ -5,7 +5,7 @@ const registerService = require(appRoot.concat('/api/services/register'));
 const registerDriver = async (requestOrSocket,responseOrUser) => {
     const isRequest = requestOrSocket.handshake == null
     const user = isRequest? {username: requestOrSocket.body.username, password: requestOrSocket.body.password}:responseOrUser;
-    const registeredUser = await registerUser({ username, password })
+    const registeredUser = await registerService(user);
     const response = registeredUser? {success: true, registeredUser,}:{success:false, error:"Could not add user"}; 
     isRequest? responseOrUser.status(200).json(response):requestOrSocket.emit("registration-response",response); 
 };
