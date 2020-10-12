7fc6db7bd2ec23c22bd4100cc7e5b25e0d70ccd2
Sandesh C <sandesh@greatbear.tech>
2020-10-11 20:23:20 -0400

    try focusing on soket events

--- server.js
+++ server.js
@@ -18,66 +18,94 @@ io.on('connection',socket => {
     console.log('Someone connected!');
 
     socket.on('registration', user => {
+        console.log(user);
         registerDriver(socket,user);
     })
 
     socket.on('login', user => {
-        console.log('loggin user')
         loginDriver(socket,user);
     })
 
-})
-
-io.of('/chat').on('connection', socket => {
-
-    const userToken = getTokenFromURL(socket.handshake.headers.referer);
-    const username = getUsernameFromToken(userToken);
-
-    // Welcome message
-    socket.emit('broadcast-message',{
-        message: "Welcome to ChatApp, ".concat(username),
-        username: "ChatApp Bot",
-    })
-
-    // Notify others of new connection
-    if (username){
-        socket.broadcast.emit('broadcast-message',{
-            message: username.concat(' has joined the chat'),
-            username: "ChatApp Bot",
-        })
-    }else{
-        socket.disconnect();
-    }
-
-    // Handle other events
-    socket.on('get-user', userToken => {
+    socket.on('get-user-onload',userToken => {
         const username = getUsernameFromToken(userToken);
-        console.log('got a request to get user:',username);
+
+        if(username){
+            // Welcome message
+            socket.emit('broadcast-message',{
+                message: "Welcome to ChatApp, ".concat(username),
+                firstname: "ChatApp Bot",
+            });
+            // Notify others of join
+            socket.broadcast.emit('broadcast-message',{
+                message: username.concat(' has joined the chat'),
+                firstname: "ChatApp Bot",
+            })
+        }else{
+            socket.disconnect();
+        }
+
         userDriver.getUser(socket,username);
-    })
 
-    socket.on('update-user', ({user, userToken}) => {
-        const username = getUsernameFromToken(userToken);
-        user.username = username;
-        userDriver.updateUser(socket,user);
     })
 
     socket.on('chat-message',msg => {
         const username = getUsernameFromToken(msg.userToken);
+        console.log('got message from',username);
         if (username){
             delete msg.userToken;
-            msg.username = username;
             socket.broadcast.emit('chat-message',msg);
         }
     })
 
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('broadcast-message',{
-            message: username.concat(' has left the chat'),
-            username: "ChatApp Bot",
-        })
-    })
 })
+
+// io.of('/chat').on('connection', socket => {
+
+    // const userToken = getTokenFromURL(socket.handshake.headers.referer);
+    // const username = getUsernameFromToken(userToken);
+
+    // // Welcome message
+
+
+    // // Notify others of new connection
+    // if (username){
+    //     socket.broadcast.emit('broadcast-message',{
+    //         message: username.concat(' has joined the chat'),
+    //         username: "ChatApp Bot",
+    //     })
+    // }else{
+    //     socket.disconnect();
+    // }
+
+//     // Handle other events
+//     socket.on('get-user', userToken => {
+//         const username = getUsernameFromToken(userToken);
+//         console.log('got a request to get user:',username);
+//         userDriver.getUser(socket,username);
+//     })
+
+//     socket.on('update-user', ({user, userToken}) => {
+//         const username = getUsernameFromToken(userToken);
+//         user.username = username;
+//         userDriver.updateUser(socket,user);
+//     })
+
+//     socket.on('chat-message',msg => {
+//         const username = getUsernameFromToken(msg.userToken);
+//         if (username){
+//             delete msg.userToken;
+//             msg.username = username;
+//             socket.broadcast.emit('chat-message',msg);
+//         }
+//     })
+
+//     socket.on('disconnect', () => {
+//         socket.broadcast.emit('broadcast-message',{
+//             message: username.concat(' has left the chat'),
+//             username: "ChatApp Bot",
+//         })
+//     })
+// })
 
 
  const getTokenFromURL = urlString => {
